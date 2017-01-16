f4093364259d45414f05ac1723df033b0efb3441
gaodalei <2542828410@qq.com>
2017-01-16 16:16:02 +0800

    11

--- vue.js/mycommit/app.js
+++ vue.js/mycommit/app.js
@@ -1,10 +1,36 @@
-var vm = new Vue({
+var apiURL = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha='
 
-	el: "#versions",
-	data:{
-		
-		branchs:["master","sdsds","dfsfdsfdsfsd"],
-		currentBranch:'master'
-	}
+/**
+ * Actual demo
+ */
 
+var demo = new Vue({
+
+  el: '#demo',
+
+  data: {
+    branches: ['master', 'dev'],
+    currentBranch: 'master',
+    commits: null
+  },
+
+  created: function () {
+    this.fetchData()
+  },
+
+  watch: {
+    currentBranch: 'fetchData'
+  },
+  methods: {
+    fetchData: function () {
+      var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', apiURL + self.currentBranch)
+      xhr.onload = function () {
+        self.commits = JSON.parse(xhr.responseText)
+        console.log(self.commits[0].html_url)
+      }
+      xhr.send()
+    }
+  }
 })
