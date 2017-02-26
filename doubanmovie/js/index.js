d1381b336d919e52d6ff99ab3778ee1c56eaceaf
gaodalei <2542828410@qq.com>
2017-02-26 23:01:06 +0800

    douban ajax

--- doubanmovie/js/index.js
+++ doubanmovie/js/index.js
@@ -5,8 +5,22 @@ $.ajax({
 	dataType:"jsonp",
 	success:function(data){
 		for (var i = 0; i < 20; i++) {
-			var url=data.subjects[i].images.large;
+			var url=data.subjects[i].images.medium;
+			$(".hot_pic").eq(i).css("background","url("+url+")");
 		}
 //		$(".hot_pic").css("background","url("+url+")");
 	}
+})
+$(function(){
+	var bimg = $(".hot_movie_bottom div");
+	var Wid = bimg.width();
+	var i=0;
+	function go(){
+			bimg.eq(i).show().animate({left:-Wid});
+			i++;
+			if (i == bimg.length) {i = 0};
+			bimg.eq(i).css("left",Wid).show().animate({left:'0px'});
+			
+		}
+		setInterval(go,2000);
 })
